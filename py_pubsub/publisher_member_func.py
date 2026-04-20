99f840362c92c7089fefe7159b1ceca12ceee6c2
AshleyN2 <ashleynjeri7@gmail.com>
2026-04-20 14:10:07 +0200

    Node Parent Class

--- py_pubsub/publisher_member_func.py
+++ py_pubsub/publisher_member_func.py
@@ -3,3 +3,15 @@ from rclpy.node import Node
 
 #from std_msgs.msg import String
 from geometry_msgs.msg import Twist 
+
+class MinimalPublisher(Node): # Node is our parent class
+    def __init__(self):
+        super.__init__('minimal_publisher') #gives our init method a name
+        self.publisher_ = self.create_publisher (Twist, 'turtlesim1/cmd_vel', 10)
+        """ Method creates a publisher 
+        Twist is the message, turtlesim1/cmd_vel is the topic & 10 is the queue length. """
+        #method timer to publish at a specific rate
+        self.timer = self.create_timer(0.5, self.timer_callback)
+
+def timer_callback(self):
+    

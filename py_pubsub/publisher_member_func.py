caa23782c0a297b61ef15e29541870744f538fa4
AshleyN2 <ashleynjeri7@gmail.com>
2026-04-20 14:30:24 +0200

    Methods added

--- py_pubsub/publisher_member_func.py
+++ py_pubsub/publisher_member_func.py
@@ -1,3 +1,4 @@
+# Turorial on https://docs.ros.org/en/jazzy/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Py-Publisher-And-Subscriber.html
 import rclpy 
 from rclpy.node import Node
 
@@ -13,5 +14,22 @@ class MinimalPublisher(Node): # Node is our parent class
         #method timer to publish at a specific rate
         self.timer = self.create_timer(0.5, self.timer_callback)
 
-def timer_callback(self):
-    
+    def timer_callback(self):
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.5
+        self.publisher_.publish(twist_msg)
+
+def main(args=None):
+    rclpy.init(args=args) #initialize ROS2 Python client library
+
+    minimal_publisher = MinimalPublisher() #create an instance of our class
+
+    rclpy.spin(minimal_publisher) #keep the node running until it is shut down
+    """
+    Destroy the node explicitly
+    (optional - otherwise it will be done automatically when the garbage collector destroys the node object)"""
+    minimal_publisher.destroy_node() #destroy the node explicitly (optional)
+    rclpy.shutdown() #shutdown the ROS2 client library
+
+if __name__ == '__main__':    
+    main()

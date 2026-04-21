2838eb78bc5c2f289bf5331865f9ffab59015b3d
AshleyN2 <ashleynjeri7@gmail.com>
2026-04-21 11:44:07 +0200

    corrected  super class

--- py_pubsub/publisher_member_func.py
+++ py_pubsub/publisher_member_func.py
@@ -7,8 +7,8 @@ from geometry_msgs.msg import Twist
 
 class MinimalPublisher(Node): # Node is our parent class
     def __init__(self):
-        super.__init__('minimal_publisher') #gives our init method a name
-        self.publisher_ = self.create_publisher (Twist, 'turtlesim1/cmd_vel', 10)
+        super().__init__('minimal_publisher') #gives our init method a name
+        self.publisher_ = self.create_publisher (Twist, 'turtle1/cmd_vel', 10)
         """ Method creates a publisher 
         Twist is the message, turtlesim1/cmd_vel is the topic & 10 is the queue length. """
         #method timer to publish at a specific rate
@@ -17,6 +17,7 @@ class MinimalPublisher(Node): # Node is our parent class
     def timer_callback(self):
         twist_msg = Twist()
         twist_msg.linear.x = 0.5
+        twist_msg.angular.z = 0.5
         self.publisher_.publish(twist_msg)
 
 def main(args=None):
